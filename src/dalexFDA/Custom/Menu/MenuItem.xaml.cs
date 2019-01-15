c28f374bc7c0c7ec9e90da9c938a500ddb3d6de5
Jesupelumi Adetunji <jadetunji@eminenttechnology.com>
2019-01-15 10:50:52 +0100

    Project renaming
    Signed-off-by: Jesupelumi Adetunji <jadetunji@eminenttechnology.com>

---
+++ src/dalexFDA/Custom/Menu/MenuItem.xaml.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Zenith
+{
+    public partial class MenuItem : ContentView
+    {
+        public MenuItem()
+        {
+            InitializeComponent();
+
+            var tap = new TapGestureRecognizer();
+            tap.Tapped += Tap_Tapped;
+            this.GestureRecognizers.Add(tap);
+        }
+
+
+        void Tap_Tapped(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.Command != null)
+                {
+                    this.Command.Execute(null);
+                }
+            }
+            catch { }
+        }
+
+
+        #region Text
+        public static readonly BindableProperty TextProperty = BindableProperty.Create("Text", typeof(string), typeof(MenuItem), default(string));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private void SetText()
+        {
+            try
+            {
+                button.Text = Text;
+            }
+            catch { }
+        }
+
+        #endregion
+
+        #region HasSubItem
+
+        public static readonly BindableProperty HasSubItemProperty = BindableProperty.Create("HasSubItem", typeof(bool), typeof(MenuItem), false);
+
+        public bool HasSubItem
+        {
+            get { return (bool)GetValue(HasSubItemProperty); }
+            set { SetValue(HasSubItemProperty, value); }
+        }
+
+        private void SetHasSubItem()
+        {
+            try
+            {
+                if (HasSubItem)
+                {
+                    caret.Svg = "res:images.icon-right-arrow";
+                    caret.IsVisible = true;
+                }
+            }
+            catch { }
+        }
+
+        #endregion
+
+        #region IsOpen
+
+        public static readonly BindableProperty IsOpenProperty = BindableProperty.Create("IsOpen", typeof(bool), typeof(MenuItem), false);
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        private void SetIsOpen()
+        {
+            try
+            {
+                if (!IsOpen)
+                {
+                    caret.Svg = "res:images.icon-right-arrow";
+                }
+                else
+                {
+                    caret.Svg = "res:images.icon-down-arrow";
+                }
+            }
+            catch { }
+        }
+
+        #endregion
+
+        #region Command
+
+        public static readonly BindableProperty CommandProperty = BindableProperty.Create("Command", typeof(Command), typeof(MenuItem), default(Command));
+
+        public Command Command
+        {
+            get { return (Command)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        private void SetCommand()
+        {
+            try
+            {
+                icon.Command = Command;
+            }
+            catch { }
+        }
+
+        #endregion
+
+        #region Icon
+
+        public static readonly BindableProperty IconProperty = BindableProperty.Create("Icon", typeof(string), typeof(MenuItem), default(string));
+
+        public string Icon
+        {
+            get { return (string)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        private void SetIcon()
+        {
+            try
+            {
+                icon.Svg = Icon;
+            }
+            catch { }
+        }
+
+        #endregion
+
+        protected override void OnPropertyChanged(string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == TextProperty.PropertyName)
+            {
+                SetText();
+            }
+
+            if (propertyName == IconProperty.PropertyName)
+            {
+                SetIcon();
+            }
+
+            if (propertyName == HasSubItemProperty.PropertyName)
+            {
+                SetHasSubItem();
+            }
+
+            if (propertyName == IsOpenProperty.PropertyName)
+            {
+                SetIsOpen();
+            }
+
+            if (propertyName == CommandProperty.PropertyName)
+            {
+                SetCommand();
+            }
+        }
+    }
+}
---
+++ src/dalexFDA/Pages/General/LeftNav/LeftNavViewModel.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Threading.Tasks;
+using PropertyChanged;
+using Xamarin.Forms;
+using Zenith.Abstractions;
+
+namespace Zenith
+{
+    [AddINotifyPropertyChangedInterface]
+    public class LeftNavViewModel: BaseViewModel
+    {
+        //default services
+        internal readonly IErrorManager ErrorManager;
+
+        //other services
+        internal readonly IAppService AppService;
+        internal readonly Acr.UserDialogs.IUserDialogs Dialog;
+
+        public bool IsMakeDepositOpen { get; set; }
+
+        //commands
+        public Command MakeDeposit { get; private set; }
+        public Command ElectronicFundTransfer { get; private set; }
+        public Command ManualBankDeposit { get; private set; }
+        public Command AccountStatements { get; private set; }
+        public Command TransactionHistory { get; private set; }
+        public Command Notifications { get; private set; }
+        public Command UpdateContactInfo { get; private set; }
+        public Command Enquiries { get; private set; }
+        public Command Feedback { get; private set; }
+        public Command MyProfile { get; private set; }
+        public Command Logout { get; private set; }
+
+        public LeftNavViewModel(IErrorManager ErrorManager, IAppService AppService, Acr.UserDialogs.IUserDialogs Dialog)
+        {
+            //setup default services
+            this.ErrorManager = ErrorManager;
+
+            //setup other services
+            this.AppService = AppService;
+            this.Dialog = Dialog;
+
+            //setup commands
+            MakeDeposit = new Command(async () => await ExecuteMakeDeposit());
+            ElectronicFundTransfer = new Command(async () => await ExecuteElectronicFundTransfer());
+            ManualBankDeposit = new Command(async () => await ExecuteManualBankDeposit());
+            AccountStatements = new Command(async () => await ExecuteAccountStatements());
+            TransactionHistory = new Command(async () => await ExecuteTransactionHistory());
+            Notifications = new Command(async () => await ExecuteNotifications());
+            UpdateContactInfo = new Command(async () => await ExecuteUpdateContactInfo());
+            Enquiries = new Command(async () => await ExecuteEnquiries());
+            Feedback = new Command(async () => await ExecuteFeedback());
+            MyProfile = new Command(async () => await ExecuteMyProfile());
+            Logout = new Command(async () => await ExecuteLogout());
+
+            Setup();
+        }
+
+        public async override void Init(object initData)
+        {
+            base.Init(initData);
+
+            try
+            {
+                IsMakeDepositOpen = false;
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+
+        private async Task ExecuteMakeDeposit()
+        {
+            try
+            {
+                IsMakeDepositOpen = !IsMakeDepositOpen;
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+
+        private async Task ExecuteElectronicFundTransfer()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+
+        private async Task ExecuteManualBankDeposit()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+
+        private async Task ExecuteAccountStatements()
+        {
+            try
+            {
+                await this.CoreMethods.DisplayAlert("BillsPaymentAndCollections", "BillsPaymentAndCollections", "OK");
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+
+        private async Task ExecuteTransactionHistory()
+        {
+            try
+            {
+                await this.CoreMethods.DisplayAlert("RecurringPayment", "RecurringPayment", "OK");
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+
+        private async Task ExecuteNotifications()
+        {
+            try
+            {
+                await this.CoreMethods.DisplayAlert("CardsAndCheques", "CardsAndCheques", "OK");
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+
+        private async Task ExecuteUpdateContactInfo()
+        {
+            try
+            {
+                await this.CoreMethods.DisplayAlert("TravelAndLeisure", "TravelAndLeisure", "OK");
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+
+        private async Task ExecuteEnquiries()
+        {
+            try
+            {
+                await this.CoreMethods.DisplayAlert("Messages", "Messages", "OK");
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+
+        private async Task ExecuteFeedback()
+        {
+            try
+            {
+                await this.CoreMethods.DisplayAlert("Settings", "Settings", "OK");
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+
+        private async Task ExecuteMyProfile()
+        {
+            try
+            {
+                await this.CoreMethods.DisplayAlert("ZenithNearMe", "ZenithNearMe", "OK");
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+
+        private async Task ExecuteLogout()
+        {
+            try
+            {
+                var title = "LOG OUT?";
+                var message = "Are you sure you want to log out?";
+                var cancelText = "Cancel";
+                var okText = "Yes";
+
+                var okTextSelected = await Dialog.ConfirmAsync(message, title, okText, cancelText);
+
+                if (okTextSelected)
+                {
+                    AppService.Logout();
+                }
+            }
+            catch (Exception ex)
+            {
+                await ErrorManager.DisplayErrorMessageAsync(ex);
+            }
+        }
+    }
+}
+
