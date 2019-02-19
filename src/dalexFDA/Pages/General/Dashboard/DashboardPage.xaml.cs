9f08e5c227f566e20a86aba5644016bef203c6bd
Jesupelumi Adetunji <jadetunji@eminenttechnology.com>
2019-02-19 21:01:41 +0100

    listview manipulation
    Signed-off-by: Jesupelumi Adetunji <jadetunji@eminenttechnology.com>

--- src/dalexFDA/Pages/General/Dashboard/DashboardPage.xaml.cs
+++ src/dalexFDA/Pages/General/Dashboard/DashboardPage.xaml.cs
@@ -24,10 +24,47 @@ namespace dalexFDA
             list.SelectedItem = null;
         }
 
+        void Handle_Appearing(object sender, System.EventArgs e)
+        {
+            try
+            {
+                var cell = sender as ViewCell;
+                var customcontrol = cell.View as InvestmentView;
+                var model = cell.BindingContext as DashboardItemViewModel;
+
+                InnerSection.TranslateTo(0, 10, 40, Easing.SinOut);
+
+                Debug.WriteLine($"Appearing InnerSection.X: {InnerSection.X}");
+                Debug.WriteLine($"Appearing InnerSection.Y: {InnerSection.Y}");
+
+                Debug.WriteLine("Cell {0} is coming onscreen", model.Investment.Id);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
 
 
+        void Handle_Disappearing(object sender, System.EventArgs e)
+        {
+            try
+            {
+                var cell = sender as ViewCell;
+                var customcontrol = cell.View as InvestmentView;
+                var model = cell.BindingContext as DashboardItemViewModel;
 
+                //InnerSection.TranslateTo(1, 0, 30, Easing.SinOut);
 
+                Debug.WriteLine($"Disappearing InnerSection.X: {InnerSection.X}");
+                Debug.WriteLine($"Disappearing InnerSection.Y: {InnerSection.Y}");
 
+                Debug.WriteLine("Cell {0} is going offscreen", model.Investment.Id);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
     }
 }
